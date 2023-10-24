92b7ac2506fcbbc96233f0aed0f6db07420dca54
mariem tijani <mariemtijani60@gmail.com>
2023-10-24 17:56:46 +0100

    array state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Calculatore from './compenents/Calculatore';
+import ArracComp from './compenents/ArracComp';
 
 
 function App() {
   
   return (
     <div className="App">
-      <Calculatore/>
+      {/* <Calculatore/> */}
+      <ArracComp/>
     </div>
   );
 }
